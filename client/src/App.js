7c6c625718333472c60bdb19ae9287f124916762
Nikhil Deenamsetty <deenamsettynikhil@gmail.com>
2020-07-08 01:33:19 -0500

    created navagation bar

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Main from './pages/Main';
 
 const App = () => {
   return (
     <Router>
-        <Switch>
-          <Route exact path='/' component={Login}/>
-          <Route exact path='/register' component={Register}/>
-        </Switch>
+      <Switch>
+        <Route exact path='/' component={Login}/>
+        <Route exact path='/register' component={Register}/>
+        <Route exact path='/main' component={Main}/>
+      </Switch>
     </Router>
-    
   );
 }
 
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../css/navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark justify-content-between nav-style">
+                <Link className="navbar-brand" style={{fontSize: "2em"}} to="/main">MyBookList</Link>
+                <div>
+                    <Link className="navbar-brand" to="/main">Account</Link>
+                    <Link className="navbar-brand" to="/">Logout</Link>
+                </div>
+        </nav>
+    );
+}
+
+export default Navbar;
