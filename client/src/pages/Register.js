ff7757fe6306d57977d23164bded55edc7007c2a
Nikhil Deenamsetty <deenamsettynikhil@gmail.com>
2020-07-28 23:57:49 -0500

    made register page responsive

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -28,6 +28,7 @@ const Register = (props) => {
 
     const onSubmit = (e) => {
         e.preventDefault();
+        // check if email already exists
         // add password == confirmPassword check
         registerUser({
             username,
@@ -44,43 +45,62 @@ const Register = (props) => {
 
     return ( // MODIFY TO ADD USERNAME
         <div className="login-imgbg">
-            <h1 className="pt-5" style={{textAlign:"center", fontSize: "7vw"}}>My Book List</h1>
-            <div className="register-box">
-                <form className="register-form" onSubmit={onSubmit}>
-                    <h1 className="mt-4">CREATE ACCOUNT</h1>
-                        <input type="text"
-                            placeholder="Username"
-                            name="username"
-                            value={username} 
-                            className="mt-3 mb-4"
-                            onChange={onChange}
-                        />
-                        <input type="email" 
-                            placeholder="Email"
-                            name="email"
-                            value={email} 
-                            className="mb-4"
-                            onChange={onChange}
-                        />
-                        <input type="password" 
-                            placeholder="Password"
-                            name="password"
-                            value={password} 
-                            className="mb-4"
-                            onChange={onChange}
-                        />
-                        <input type="password" 
-                            placeholder="Confirm Password"
-                            name="confirmPassword"
-                            value={confirmPassword} 
-                            className="mb-4"
-                            onChange={onChange}
-                        />
-                    <div className="register-button">
-                        <input type="submit" value="Register" className="btn btn-dark"/>
-                    </div>
-                    <Link className="link mt-3" to="/">Already Have An Account?</Link>
-                </form>
+            <h1 className="login-heading pt-5" style={{textAlign:"center"}}>My Book List</h1>
+            <div className="mt-4" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                <div className="test">
+                    <form onSubmit={onSubmit}>
+                        <div className="col">
+                            <div className="form-group mt-3">
+                                    <input 
+                                        type="text" 
+                                        name="username" 
+                                        value={username}
+                                        className="form-control" 
+                                        placeholder="Username"
+                                        onChange={onChange}
+                                    />
+                            </div>
+                            <div className="form-group mt-3">
+                                <input 
+                                    type="email" 
+                                    name="email" 
+                                    value={email}
+                                    className="form-control" 
+                                    placeholder="Email"
+                                    onChange={onChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <input 
+                                    type="password" 
+                                    name="password" 
+                                    value={password}
+                                    className="form-control" 
+                                    placeholder="Password"
+                                    onChange={onChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <input 
+                                    type="confirmPassword" 
+                                    name="confirmPassword" 
+                                    value={confirmPassword}
+                                    className="form-control" 
+                                    placeholder="Confirm Password"
+                                    onChange={onChange}
+                                />
+                            </div>
+                            <div>
+                                <button type="submit" className="btn btn-dark btn-block">Register</button>
+                            </div>
+                            <Link to="/" 
+                            className="mt-3"
+                            style={{display: "flex", justifyContent: "center", textDecoration: "none", color: "white"}}>
+                                Already Have An Account?
+                            </Link>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
