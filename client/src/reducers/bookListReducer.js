48ac0d597fda12488b0d2d0365ad5f051e28e9b5
Nikhil Deenamsetty <deenamsettynikhil@gmail.com>
2020-07-29 20:06:08 -0500

    update book status after filter working properly

--- client/src/reducers/bookListReducer.js
+++ client/src/reducers/bookListReducer.js
@@ -41,29 +41,32 @@ export const bookListReducer = (state, action) => {
             ...state,
             finishedList: state.finishedList.some(book => book.bookId === action.payload.bookId) ? state.finishedList : [...state.finishedList, action.payload],
             readingList: state.readingList.filter(book => book.bookId !== action.payload.bookId),
-            favorites: state.favorites.filter(book => book.bookId !== action.payload.bookId)
+            favorites: state.favorites.filter(book => book.bookId !== action.payload.bookId),
+            filtered: state.filtered.filter(book => book.bookId !== action.payload.bookId)
           }
         case 'UPDATE_FAVORITE':
           return {
             ...state,
             favorites: state.favorites.some(book => book.bookId === action.payload.bookId) ? state.favorites : [...state.favorites, action.payload],
             readingList: state.readingList.filter(book => book.bookId !== action.payload.bookId),
-            finishedList: state.finishedList.filter(book => book.bookId !== action.payload.bookId)
+            finishedList: state.finishedList.filter(book => book.bookId !== action.payload.bookId),
+            filtered: state.filtered.filter(book => book.bookId !== action.payload.bookId)
           }
         case 'UPDATE_READING':
           return {
             ...state,
             readingList: state.readingList.some(book => book.bookId === action.payload.bookId) ? state.readingList : [...state.readingList, action.payload],
             favorites: state.favorites.filter(book => book.bookId !== action.payload.bookId),
-            finishedList: state.finishedList.filter(book => book.bookId !== action.payload.bookId)
+            finishedList: state.finishedList.filter(book => book.bookId !== action.payload.bookId),
+            filtered: state.filtered.filter(book => book.bookId !== action.payload.bookId)
           }
         case 'REMOVE_BOOK':
           return {
             ...state,
             readingList: state.readingList.filter(book => book.bookId !== action.payload),
             favorites: state.favorites.filter(book => book.bookId !== action.payload),
-            finishedList: state.finishedList.filter(book => book.bookId !== action.payload)
-
+            finishedList: state.finishedList.filter(book => book.bookId !== action.payload),
+            filtered: state.filtered.filter(book => book.bookId !== action.payload)
           }
         case 'ADD_FAVORITE':
           return {
